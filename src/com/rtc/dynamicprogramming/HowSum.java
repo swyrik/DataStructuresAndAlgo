e123fbf0d3fa5ae3c31cfc34af06d2a028b99e82
AG42993 <Swyrik.Thupili@legatohealth.com>
2021-06-07 21:47:05 +0530

    how sum

---
+++ src/com/rtc/dynamicprogramming/HowSum.java
@@ -0,0 +1,50 @@
+package com.rtc.dynamicprogramming;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class HowSum {
+
+    public static void main(String[] args){
+        System.out.println(canSum(7, new int[]{2,3}, new LinkedList<Integer>(), new HashMap<>()));
+        System.out.println(canSum(7, new int[]{5,3,4,7}, new LinkedList<Integer>(), new HashMap<>()));
+        System.out.println(canSum(7, new int[]{2,4}, new LinkedList<Integer>(), new HashMap<>()));
+        System.out.println(canSum(8, new int[]{2,3,5}, new LinkedList<Integer>(), new HashMap<>()));
+        System.out.println(canSum(300, new int[]{7,14}, new LinkedList<Integer>(), new HashMap<>()));
+    }
+
+    private static List<Integer> canSum(int targetNum, int[] arr, List<Integer> list, Map<Integer, List<Integer>> map){
+        if(map.containsKey(targetNum)){
+            list.remove(list.size()-1);
+            return map.get(targetNum);
+        }
+
+
+
+
+        if(targetNum == 0){
+            return list;
+        }
+
+        if(targetNum < 0){
+            list.remove(list.size()-1);
+            return null;
+        }
+
+        for (int num: arr) {
+            int newTargetNum = targetNum - num;
+            list.add(num);
+//            System.out.println("targetNum: "+targetNum +", num: "+ num + ", newTargetNum: "+newTargetNum);
+            List<Integer> newList = canSum(newTargetNum, arr, list, map);
+            map.put(newTargetNum, newList);
+            if(null != newList){
+                return newList;
+            }
+        }
+        if(list.size() > 0)
+            list.remove(list.size()-1);
+        return null;
+    }
+}
